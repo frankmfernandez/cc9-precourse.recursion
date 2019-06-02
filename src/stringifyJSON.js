c6c7b9229bbaefe20d675b464719b2d9fddecef4
TJ Epperson <tj.epperson070@gmail.com>
2019-06-02 15:16:31 +0900

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,6 +6,35 @@ What does the output for stringifyJSON look like? Play around with JSON.stringif
 
 */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (x) => {
+  let returnArray = [];
+  if (x === undefined)
+    return undefined;
+  if (typeof x === 'function')
+    return undefined;
+  if (x === null)
+    return 'null';
+  if (typeof x === 'string')
+    return `"${x}"`;
+  if (typeof x === 'object' && x.constructor === Object) {
+    for (let key in x) {
+      let keyStr = stringifyJSON(key);
+      let valStr = stringifyJSON(x[key]);
+      if (keyStr && valStr) {
+        returnArray.push(`${keyStr}:${valStr}`)
+      }
+    }
+    return `{${returnArray.join(',')}}`;
+  } else if (Array.isArray(x)) {
+    if (x.length === 0)
+      return '[]';
+    else {
+      x.forEach((item) => {
+        returnArray.push(stringifyJSON(item));
+      });
+      return `[${returnArray}]`;
+    }
+  } else {
+    return x.toString();
+  }
 };

9a067a23a3fac33f69c375548f892c3c1b986467
TJ Epperson <tj.epperson070@gmail.com>
2019-06-04 17:00:33 +0900

    Added treeSearcher

---
+++ src/treeSearcher.js
@@ -0,0 +1,57 @@
+const treeMaker = (data, parent) => {
+    let node = {};
+    data
+        .filter(x => x.parent === parent)
+        .forEach(x => node[x.name] = treeMaker(data, x.name));
+    return node;
+}
+
+let testData = [
+    {name: 'Uppercase', parent: null},
+    {name: 'Lowercase', parent: null},
+    {name: 'A', parent: 'Uppercase'},
+    {name: 'a', parent: 'Lowercase'},
+    {name: 'Apple', parent: 'A'},
+    {name: 'apple', parent: 'a'},
+    {name: 'Ant', parent: 'A'},
+    {name: 'ant', parent: 'a'},
+    {name: 'B', parent: 'Uppercase'},
+    {name: 'b', parent: 'Lowercase'},
+    {name: 'Big', parent: 'B'},
+    {name: 'big', parent: 'b'},
+    {name: 'Boring', parent: 'B'},
+    {name: 'boring', parent: 'b'},
+    {name: 'C', parent: 'Uppercase'},
+    {name: 'c', parent: 'Lowercase'},
+    {name: 'Cow', parent: 'C'},
+    {name: 'cow', parent: 'c'},
+    {name: 'Car', parent: 'C'},
+    {name: 'car', parent: 'c'},
+];
+
+const tree = treeMaker(testData, null);
+
+const treeSearcher = (tree, target) => {
+    for (let key in tree) {
+        if (key === target) {
+            return key;
+        }
+        if (tree[key] === target) {
+            return tree[key];
+        }
+        if (Object.keys(tree[key]).length > 0) {
+            return treeSearcher(tree[key], target);
+        }
+    }
+    return 'NULL';
+    /*
+    for (let key in tree) {
+        console.log(`Key: ${key} & Value: ${tree[key]}`);
+        console.log(Object.keys(tree[key]).length);
+    }
+    */
+}
+
+//console.log(treeSearcher(tree, 'Apple'));
+console.log(JSON.stringify(tree, null, 2));
+console.log(treeSearcher(tree, 'Apple'));

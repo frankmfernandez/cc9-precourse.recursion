6512c909de38a2dbf68bed9b1610e02ab0c226b3
Frank Fernandez <frankfernandez@Franks-Macbook.local>
2019-06-07 10:44:06 +0900

    Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,6 +6,37 @@ What does the output for stringifyJSON look like? Play around with JSON.stringif
 
 */
 
-const stringifyJSON = () => {
+const stringifyJSON = (stringMe) => {
   // YOUR CODE HERE
+  '\'<option value="1">tea</option>\''
+  if (typeof stringMe === "string"){
+    return `"${stringMe}"`
+  } else if (Array.isArray(stringMe)){
+     let stringedElement = '[' + stringMe.reduce((acc, v) => {
+      if (v === undefined)
+        return [...acc, 'null']
+      else
+        return [...acc, stringifyJSON(v)]
+    }, []).join(',') + ']'
+    return stringedElement
+  }else if (typeof stringMe === "object" && stringMe !== null && (!Array.isArray(stringMe))){
+    if(stringMe.hasOwnProperty("functions") || stringMe.hasOwnProperty("undefined")){
+      return '{'+'}';
+    }
+    let stringedElement = '{' + Object.keys(stringMe).reduce((acc, k) => {
+      if (stringMe[k] === undefined)
+        return acc
+      else
+        return [...acc, stringifyJSON(k) + ':' + stringifyJSON(stringMe[k])]
+    }, []).join(',') + '}'
+    return stringedElement
+  } else if (typeof stringMe === "undefined"){
+    return {};
+  }else {
+    let stringedElement =  "\'" + stringMe +"\'"
+    stringedElement = stringedElement.slice(1,-1)
+    
+      return stringedElement;
+  }
 };
+
